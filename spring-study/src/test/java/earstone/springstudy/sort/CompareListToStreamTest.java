ed085d80bf4c89980c88b0501e62b42f61b9341f
chionsuk <hsuk0409@gmail.com>
2022-05-23 22:52:25 +0900

    array sort vs stream sort

--- spring-study/src/test/java/earstone/springstudy/sort/CompareListToStreamTest.java
+++ spring-study/src/test/java/earstone/springstudy/sort/CompareListToStreamTest.java
@@ -1,2 +1,46 @@
-package earstone.springstudy.sort;public class CompareListToStreamTest {
+package earstone.springstudy.sort;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class CompareListToStreamTest {
+
+    @DisplayName("100만까지 랜덤수를 list 활용하여 내림차순 정렬한다.")
+    @Test
+    void listSortTest() {
+        //given
+        List<Integer> list = new ArrayList<>();
+
+        //when
+        Random random = new Random();
+        for (int i = 0; i < 1000000; ++i) {
+            list.add(random.nextInt());
+        }
+
+        //then
+        Collections.sort(list);
+        System.out.println(list.get(0));
+    }
+
+    @DisplayName("100만까지 랜덤수를 stream 활용하여 내림차순 정렬한다.")
+    @Test
+    void streamSortTest() {
+        //given
+        List<Integer> list = new ArrayList<>();
+
+        //when
+        Random random = new Random();
+        for (int i = 0; i < 1000000; ++i) {
+            list.add(random.nextInt());
+        }
+
+        //then
+        System.out.println(list.stream()
+                .sorted(Comparator.comparing(dto -> dto))
+                .collect(Collectors.toList()).get(0));
+    }
+
 }
